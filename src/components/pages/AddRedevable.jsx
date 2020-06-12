eb546300bf33747d22d1d0db52d0a85e8b3916f8
Youssef Angri <star.angri@gmail.com>
2020-06-12 02:37:15 +0100

    Add new Redevable by POST method

--- src/components/pages/AddRedevable.jsx
+++ src/components/pages/AddRedevable.jsx
@@ -3,52 +3,92 @@ import React, { Component } from "react";
 class AddRedevable extends Component {
   constructor(props) {
     super(props);
-    this.handleSave = this.handleSave.bind(this);
-    this.state = { redevable: [] };
+    this.handleChangeCin = this.handleChangeCin.bind(this);
+    this.handleChangeNom = this.handleChangeNom.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { cin: "", nom: "" };
   }
 
-  handleSave(event) {
+  handleChangeCin(event) {
+    this.setState({ cin: event.target.value });
+  }
+  handleChangeNom(event) {
+    this.setState({ nom: event.target.value });
+  }
+
+  handleSubmit(event) {
+    //alert("Le nom " + this.state.nom + "Le cin " + this.state.cin);
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        cin: this.state.cin,
+        nom: this.state.nom,
+      }),
+    };
+    fetch("http://localhost:8090/taxe-tnb/redevable/", requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data > 0) {
+          alert("Redebale ajouter avec succes");
+        } else {
+          alert("Erreur");
+        }
+      });
     event.preventDefault();
-    console.log(event);
   }
 
   render() {
     return (
-      <div className="card card-primary">
-        <div className="card-header">
-          <h3 className="card-title">Paye Taxe</h3>
-        </div>
-        {/* /.card-header */}
-        {/* form start */}
-        <form role="form">
-          <div className="card-body">
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Redevable CIN</label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputCIN"
-                placeholder="Enter CIN"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Redevable Name</label>
-              <input
-                type="text"
-                className="form-control"
-                id="inputName"
-                placeholder="Enter CIN"
-              />
-            </div>
-          </div>
+      <div className="content">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-12">
+              <div className="card card-primary">
+                <div className="card-header">
+                  <h3 className="card-title">Paye Taxe</h3>
+                </div>
+                {/* /.card-header */}
+                {/* form start */}
+                <form role="form" onSubmit={this.handleSubmit}>
+                  <div className="card-body">
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Redevable CIN</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="inputCIN"
+                        placeholder="Enter CIN"
+                        value={this.state.cin}
+                        onChange={this.handleChangeCin}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Redevable Name</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="inputNom"
+                        placeholder="Enter Name"
+                        value={this.state.nom}
+                        onChange={this.handleChangeNom}
+                      />
+                    </div>
+                  </div>
 
-          {/* /.card-body */}
-          <div className="card-footer">
-            <button onClick={this.handleSave} className="btn btn-primary">
-              save
-            </button>
+                  {/* /.card-body */}
+                  <div className="card-footer">
+                    <button type="submit" className="btn btn-primary">
+                      save
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-        </form>
+        </div>
       </div>
     );
   }
